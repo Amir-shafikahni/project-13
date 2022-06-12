96e5817fe63e9e26e6e437afe45613b2c2880b68
Amir_sh <mrshfkhn@gmail.com>
2022-06-12 20:55:02 +0430

    first commit

---
+++ Source/script.js
@@ -0,0 +1,30 @@
+let $ = document
+
+///////////////////////////
+
+const body = $.body
+const mainBody = $.querySelector(".mainBody")
+const modalBox = $.querySelector(".modalBox")
+const modalOpenerBtn = $.querySelector(".mainBody .btn")
+const modalCloserBtn = $.querySelector(".modalBox .btn-close")
+
+modalOpenerBtn.addEventListener("click" , function(){
+    modalBox.className = "modalBox fade-in"
+    mainBody.style.filter = "blur(2px)"
+
+    body.addEventListener("keydown" , modalKeyboardCloser)
+})
+
+modalCloserBtn.addEventListener("click" , function(){
+    modalBox.className = "modalBox fade-out"
+    mainBody.style.filter = "blur(0px)"
+    body.removeEventListener("keydown" , modalKeyboardCloser)
+})
+
+function modalKeyboardCloser(event){
+    if(event.key === "Escape"){
+        modalBox.className = "modalBox fade-out"
+        mainBody.style.filter = "blur(0px)"
+        body.removeEventListener("keydown" , modalKeyboardCloser)
+    }
+}
